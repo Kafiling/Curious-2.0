77c5f07002bdfdbde323cc7bf650faf92e94823a
Kafiling <kafilkafilth@gmail.com>
2022-06-12 10:03:52 +0700

    Auth

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -9,8 +9,38 @@ import { IconContext } from 'react-icons/lib'
 import {Button} from './../Button'
 import './Login.css'
 
+import {auth} from './../../../Firebase'
+import {signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
 function Login() {
+
+  const signInWithGoogle = () =>{
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+    .then((re) => {
+      console.log(re)
+    })
+    .catch((err) => {
+      console.log(err)
+      if (err.code === 'auth/account-exists-with-different-credential') {
+        alert('บัญชีของคุณที่ผูกกับ Facebook ไว้แล้ว กรุณา login ผ่าน Facebook')
+      }
+    })
+  }
+  const signInWithFacebook = () =>{
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(auth, provider)
+    .then((re) => {
+      console.log(re)
+    })
+    .catch((err) => {
+      console.log(err)
+      if (err.code === 'auth/account-exists-with-different-credential') {
+        alert('บัญชีของคุณที่ผูกกับ Google ไว้แล้ว กรุณา login ผ่าน Google')
+      }
+    })
+  }
+
   return (
     <div className='login-container'>
     <IconContext.Provider value={{colr : '#fff'}}>
@@ -21,8 +51,8 @@ function Login() {
       
       <br/><br/><br/>
       <div>
-      <Button buttonSize = 'btn--wide'  buttonColor='blue' ><BsGoogle className='navbar-icon'  />Login with google&#160;&#160;&#160;</Button><br/><br/><br/><br/>
-      <Button buttonSize = 'btn--wide'  buttonColor='blue' ><BsFacebook className='navbar-icon'  />Login with facebook</Button>
+      <Button buttonSize = 'btn--wide'  buttonColor='blue' onClick={() =>signInWithGoogle()}><BsGoogle className='navbar-icon'  />Login with google&#160;&#160;&#160;</Button><br/><br/><br/><br/>
+      <Button buttonSize = 'btn--wide'  buttonColor='blue' onClick={() =>signInWithFacebook()}><BsFacebook className='navbar-icon'  />Login with facebook</Button>
       </div><br/><br/><br/>
       <p>"The roots of education are bitter, but the fruit is sweet." - Aristotle</p><br/>
       <a href='https://keep-curious.web.app/'><p>คลิกที่นี้เพื่อเข้าถึง Curious v1.0</p></a>
