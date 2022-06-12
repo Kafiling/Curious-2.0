b80b967a2e56f8338d0164f741a551bed3615d12
Kafiling <kafilkafilth@gmail.com>
2022-06-12 21:31:17 +0700

    State Nav

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import {AiTwotoneExperiment} from 'react-icons/ai'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import {Button} from './Button'
 import { IconContext } from 'react-icons/lib'
 import './Navbar.css'
+import {auth,AuthContext} from './../../Firebase'
 
 function Navbar(){
+    const {currentUser} = useContext(AuthContext)
+    return(currentUser? <NavbarLogin/> : <NavbarNotLogin/>)
+}
+function NavbarNotLogin(){
     const [click, setClick] = useState(false)
     const [button, setBotton] = useState(true)
 
@@ -69,6 +74,81 @@ function Navbar(){
                         ): (
                             <Link to= '/login' className="btn-link" onClick={closeMobileMenu}>
                                 <Button buttonStyle = 'btn--outline' buttonSize ='btn--mobile'> Get started</Button>
+                            </Link>
+                        )}
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+        </IconContext.Provider>
+        </>
+    )
+}
+
+function NavbarLogin(){
+    const [click, setClick] = useState(false)
+    const [button, setBotton] = useState(true)
+
+    const handleClick = ()=> setClick(!click)
+    const closeMobileMenu = ()=> setClick(false)
+
+    const showButton = () => {
+        if(window.innerWidth <= 650) {
+            setBotton(false)
+        }
+
+        else{
+            setBotton(true)
+        }
+    }
+
+    useEffect(() => {
+        showButton()
+    }, [])
+ 
+    window.addEventListener('resize',showButton) 
+    return (
+        <>
+        <IconContext.Provider value={{color : '#fff'}}>
+        <div className="navbar">
+            <div className="navbar-container container"> 
+                <Link to= '/' className="navbar-logo" onClick={closeMobileMenu}>
+                <AiTwotoneExperiment className='navbar-icon' />
+                Curious
+                </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                {click ? <FaTimes/> : <FaBars/>}
+                </div>
+                <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to= '/courses' className="nav-links" onClick={closeMobileMenu}>
+                        Courses
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to= '/playground' className="nav-links" onClick={closeMobileMenu}>
+                            Playground
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to= '/aboutUs' className="nav-links" onClick={closeMobileMenu}>
+                            About Us
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to= '/help' className="nav-links" onClick={closeMobileMenu}>
+                            Help
+                        </Link>
+                    </li>
+                    <li className="nav-btn">
+                        {button ? (
+                            <Link to= '/' className="btn-link" >
+                                <Button buttonStyle = 'btn--outline' onClick={() =>  auth.signOut()}> Sign Out</Button>
+                            </Link>
+                        ): (
+                            <Link to= '/' className="btn-link" onClick={closeMobileMenu}>
+                                <Button buttonStyle = 'btn--outline' buttonSize ='btn--mobile' onClick={() =>  auth.signOut()}> Sign Out</Button>
                             </Link>
                         )}
                     </li>
